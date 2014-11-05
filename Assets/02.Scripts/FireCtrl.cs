dfdbf4505bdd7efa2a477fe8c7540194b97351b0
rapker <rapker7@gmail.com>
2014-11-05 23:13:48 +0900

    연습 추가

---
+++ Assets/02.Scripts/FireCtrl.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FireCtrl : MonoBehaviour {
+
+	public GameObject bullet;
+	public Transform firePos;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetMouseButtonDown (0)) {
+			Fire();
+		}
+	}
+
+	private void Fire(){
+		StartCoroutine (this.CreateBullet());
+	}
+
+	IEnumerator CreateBullet(){
+		Instantiate (bullet, firePos.position, firePos.rotation);
+		yield return null;
+	}
+}
--- Assets/02.Scripts/PlayerCtrl.cs
+++ Assets/02.Scripts/PlayerCtrl.cs
@@ -5,14 +5,31 @@ public class PlayerCtrl : MonoBehaviour {
 	private float h = 0.0f;
 	private float v = 0.0f;
 
+	private float fCrossFadeTime = 0.3f;
+
 	private Transform tr;
 
 	public float moveSpeed = 10.0f;
 	public float rotSpeed = 100.0f;
 
+	[System.Serializable]
+	public class Anim{
+		public AnimationClip idle;
+		public AnimationClip runForward;
+		public AnimationClip runBackward;
+		public AnimationClip runRight;
+		public AnimationClip runLeft;
+	}
+
+	public Anim anim;
+
+	public Animation _animation;
+
 	// Use this for initialization
 	void Start () {
 		tr = GetComponent <Transform>();
+
+		Init_Animation ();
 	}
 	
 	// Update is called once per frame
@@ -23,10 +40,35 @@ public class PlayerCtrl : MonoBehaviour {
 		Debug.Log ("H = " + h.ToString ());
 		Debug.Log ("V = " + v.ToString ());
 
+		Update_Move ();
+		Update_Rotate ();
+		Update_Animation ();
+	}
+
+	private void Init_Animation(){
+		_animation = GetComponentInChildren<Animation> ();
+		_animation.clip = anim.idle;
+		_animation.Play ();
+	}
+
+	private void Update_Move(){
 		Vector3 moveDir = ( Vector3.forward* v ) + (Vector3.right * h);
-
 		tr.Translate( moveDir * Time.deltaTime * moveSpeed, Space.Self );
-
+	}
+	private void Update_Rotate(){
 		tr.Rotate( Vector3.up * Time.deltaTime * rotSpeed * Input.GetAxis("Mouse X"));
+	}
+	private void Update_Animation(){
+		if (v >= 0.1f) {
+			_animation.CrossFade (anim.runForward.name, fCrossFadeTime);
+		} else if (v < 0.0f) {
+			_animation.CrossFade (anim.runBackward.name, fCrossFadeTime);
+		} else if (h >= 0.1f) {
+			_animation.CrossFade (anim.runRight.name, fCrossFadeTime);
+		} else if (h < 0.0f) {
+			_animation.CrossFade (anim.runLeft.name, fCrossFadeTime);
+		} else {
+			_animation.CrossFade (anim.idle.name, fCrossFadeTime);
+		}
 	}
 }
